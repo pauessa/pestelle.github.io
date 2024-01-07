02ac7b10314c165174ba68fa7c15d1678cc8eb1d
pestelle <pauessa@gmail.com>
2024-01-07 13:08:16 +0100

    add login

--- login.js
+++ login.js
@@ -5,22 +5,25 @@ function validateLogin() {
 
     // Usa fetch para cargar el archivo
     fetch(rutaArchivo)
-      .then(response => {
-        // Verifica si la solicitud fue exitosa (código de estado 200)
-        if (!response.ok) {
-          throw new Error('No se pudo cargar el archivo.');
-        }
-        // Devuelve el cuerpo de la respuesta como texto
-        return response.text();
-      })
-      .then(contenido => {
-        // Haz algo con el contenido (en este caso, imprímelo en la consola)
-        console.log(contenido);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-   
+        .then(response => {
+            // Verifica si la solicitud fue exitosa (código de estado 200)
+            if (!response.ok) {
+                throw new Error('No se pudo cargar el archivo.');
+            }
+            // Devuelve el cuerpo de la respuesta como objeto JSON
+            return response.json();
+        })
+        .then(validCredentials => {
+            // Verifica si las credenciales proporcionadas son válidas
+            if (isValidCredentials(username, password, validCredentials)) {
+                console.log('Credenciales válidas. Acceso permitido.');
+            } else {
+                console.log('Credenciales no válidas. Acceso denegado.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 function isValidCredentials(username, password, validCredentials) {
